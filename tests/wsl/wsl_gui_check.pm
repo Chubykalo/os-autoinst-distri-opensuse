04855d594a757fedc4356e3d026ed52c4a363159
Evgueni Tchubykalo <etchubykalo@suse.com>
2024-12-10 10:51:11 +0100

    Create wsl_gui_ckeck
    Improving WSL GUI testing.

---
+++ tests/wsl/wsl_gui_check.pm
@@ -0,0 +1,21 @@
+# SUSE's openQA tests
+#
+# Copyright 2012-2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: edit this here!
+# Maintainer: qa-c <qa-c@suse.de>
+
+use Mojo::Base qw(windowsbasetest);
+use testapi;
+use version_utils qw(is_sle is_opensuse);
+
+# install pattern wsl_gui
+# install mousepad
+# check that mousepad works
+
+sub run {
+    my ($self) = @_;
+
+    assert_screen(['windows-desktop', 'powershell-as-admin-window']);
+}
